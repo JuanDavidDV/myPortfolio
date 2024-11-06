d2a8028593240e0c3a3108eff39e092dcaa91ad4
JuanDavidDV <juandadazav@gmail.com>
2024-11-06 10:42:05 -0500

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Content from "./components/Content";
 
 function App() {
-
 
   return (
     <BrowserRouter>
       <NavBar />
+      <Home />
     </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Content from "../components/Content";
+
+const Home = () => {
+  return (
+    <main>
+
+    </main>
+  )
+
+}
+
+export default Home;
