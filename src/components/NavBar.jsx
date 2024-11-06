09b8514c54b14359dc3922994872324cd50db10d
JuanDavidDV <juandadazav@gmail.com>
2024-11-06 10:03:44 -0500

    finalized navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,33 +4,38 @@ import { useState } from "react";
 const NavBar = () => {
   const [menuStatus, setMenuStatus] = useState(null);
 
-  const navLinks = document.querySelector(".nav-links");
+  console.log(menuStatus)
 
-  const toggleMenu = () => {
+    const toggleMenu = () => {
     const menu = document.getElementById("toggle-menu");
     menu.name = menu.name === "menu" ? "close" : "menu";
-    navLinks.classList.toggle("top-[9%]")
+      if(menu.name === "close") {
+        setMenuStatus("top-[53px]");
+      } else {
+        setMenuStatus(null);
+      }
+      
   }
 
   return (
-    <header className="bg-zinc-700 py-3">
+    <header className="bg-zinc-600 py-3">
       <nav className="flex justify-between items-center w-[92%] mx-auto">
         <div>
-          <button className="text-white rounded-lg font-bold p-2 text-center bg-orange-500 hover:brightness-125 transition-all duration-150">Juan David DV</button>
+          <button className="text-white rounded-lg font-bold px-1 py-0.5 text-center bg-orange-500 hover:brightness-125 transition-all duration-150">Juan David DV</button>
         </div>
-        <div className="nav-links md:static absolute bg-zinc-700 md:min-h-fit min-h-80 left-0 top-[-100%] md:w-auto w-full flex items-center px-5 ">
+        <div className={`${menuStatus} duration-300 md:static absolute bg-zinc-600 md:min-h-fit min-h-80 left-0 top-[-100%] md:w-auto w-full flex items-center px-5`}>
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-12">
             <li>
-              <a className="text-white p-1.5 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">About</a>
+              <a className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">About</a>
             </li>
             <li>
-              <a className="text-white p-1.5 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">Projects</a>
+              <a className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">Projects</a>
             </li>
             <li>
-              <a className="text-white p-1.5 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">Experience</a>
+              <a className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">Experience</a>
             </li>
             <li>
-              <NavLink to="#" className="text-white p-1.5 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350">
+              <NavLink to="#" className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350">
                 Contact
               </NavLink>
             </li>
