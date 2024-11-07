26c5f289cce4bccd971904c1fd839e15206ae38b
JuanDavidDV <juandadazav@gmail.com>
2024-11-07 16:57:54 -0500

    fix navbar current location

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
 const NavBar = () => {
-  const [menuStatus, setMenuStatus] = useState(null);
 
-  console.log(menuStatus)
+  const location = useLocation();
+  const isAbout = location.pathname === "/";
+  const isProjects = location.pathname === "/projects";
+  const isExperience = location.pathname === "/experience";
+  const isContact = location.pathname === "/contact";
+
+  const [menuStatus, setMenuStatus] = useState(null);
 
     const toggleMenu = () => {
     const menu = document.getElementById("toggle-menu");
@@ -28,20 +33,20 @@ const NavBar = () => {
         <div className={`${menuStatus} duration-300 md:static absolute bg-zinc-600 md:min-h-fit min-h-80 left-0 top-[-100%] md:w-auto w-full flex items-center px-5`}>
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-12">
             <li>
-              <Link to="/" className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350">
+              <Link to="/" className={`text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350 ${isAbout ? "bg-blue-500 rounded-lg font-bold" : ""}`}>
                 About
               </Link>
             </li>
             <li>
-              <Link to="/projects" className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350">
+              <Link to="/projects" className={`text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350 ${isProjects ? "bg-blue-500 rounded-lg font-bold" : ""}`}>
                 Projects
               </Link>
             </li>
             <li>
-              <Link to="/experience" className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350" href="#">Experience</Link>
+              <Link to="/experience" className={`text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350 ${isExperience ? "bg-blue-500 rounded-lg font-bold" : "" }`}>Experience</Link>
             </li>
             <li>
-              <Link to="/contact" className="text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350">
+              <Link to="/contact" className={`text-white px-2.5 py-1 hover:bg-blue-500 rounded-lg font-semi-bold transition-all duration-350 ${isContact ? "bg-blue-500 rounded-lg font-bold" : ""}`}>
                 Contact
               </Link>
             </li>
