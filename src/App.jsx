4a9212369ea1db11eba9c6aab0ec9dc024f4d396
JuanDavidDV <juandadazav@gmail.com>
2024-11-07 08:50:09 -0500

    technology section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Projects from "./components/Projects";
 import Content from "./components/About";
 
 function App() {
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { RiReactjsLine } from "react-icons/ri";
 import github from "../assets/images/github.png";
 import linkedin from "../assets/images/linkedIn.png";
@@ -9,6 +10,13 @@ import { FaNodeJs } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { IoLogoGithub } from "react-icons/io";
 import { FaGitAlt } from "react-icons/fa6";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaSass } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiPostman } from "react-icons/si";
+import { GrHeroku } from "react-icons/gr";
+import { BiLogoNetlify } from "react-icons/bi";
 
 const About = () => {
   return (
@@ -40,43 +48,66 @@ const About = () => {
         </div>
       </div>
       <div className="pb-24">
-        <h2 className="mt-20 mb-10 text-center text-5xl font-bold">
+        <h2 className="mt-20 mb-10 text-center text-4xl md:text-5xl text-gray-800 font-bold">
           Technologies
         </h2>
         <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="p-4">
-            <SiRubyonrails className="text-7xl text-red-500"/>
+          <div className="p-2 md:p-2 md:p-4">
+            <SiRubyonrails className="text-5xl md:text-7xl text-red-500"/>
           </div>
-          <div className="p-4">
-            <RiReactjsLine className="text-7xl text-cyan-400" />
+          <div className="p-2 md:p-4">
+            <RiReactjsLine className="text-5xl md:text-7xl text-cyan-400" />
           </div>
-          <div className="p-4">
-            <SiJavascript className="text-7xl text-yellow-400"/>
+          <div className="p-2 md:p-4">
+            <SiJavascript className="text-5xl md:text-7xl text-yellow-400"/>
           </div>
-          <div className="p-4">
-            <SiMicrosoftsqlserver className="text-7xl text-red-400" />
+          <div className="p-2 md:p-4">
+            <SiMicrosoftsqlserver className="text-5xl md:text-7xl text-red-400" />
           </div>
-          <div className="p-4">
-            <BiLogoPostgresql className="text-7xl text-blue-800" />
+          <div className="p-2 md:p-4">
+            <BiLogoPostgresql className="text-5xl md:text-7xl text-blue-800" />
           </div>
-          <div className="p-4">
-            <FaNodeJs className="text-7xl text-green-500" />
+          <div className="p-2 md:p-4">
+            <FaNodeJs className="text-5xl md:text-7xl text-green-500" />
           </div>
-          <div className="p-4">
-            <SiExpress className="text-7xl"/>
+          <div className="p-2 md:p-4">
+            <SiExpress className="text-5xl md:text-7xl"/>
           </div>
-          <div className="p-4">
-            <IoLogoGithub className="text-7xl" />
+          <div className="p-2 md:p-4">
+            <IoLogoGithub className="text-5xl md:text-7xl" />
           </div>
-          <div className="p-4">
-            <FaGitAlt className="text-7xl text-orange-600" />
+          <div className="p-2 md:p-4">
+            <FaGitAlt className="text-5xl md:text-7xl text-orange-600" />
           </div>
-
-
+          <div className="p-2 md:p-4">
+            <FaHtml5 className="text-5xl md:text-7xl text-orange-500" />
+          </div>
+          <div className="p-2 md:p-4">
+            <IoLogoCss3 className="text-5xl md:text-7xl text-blue-600"/>
+          </div>
+          <div className="p-2 md:p-4">
+            <FaSass className="text-5xl md:text-7xl text-pink-500"/>
+          </div>
+          <div className="p-2 md:p-4">
+            <RiTailwindCssFill className="text-5xl md:text-7xl text-cyan-700"/>
+          </div>
+          <div className="p-2 md:p-4">
+            <SiPostman className="text-5xl md:text-7xl text-orange-500" />
+          </div>
+          <div className="p-2 md:p-4">
+            <GrHeroku className="text-5xl md:text-7xl text-purple-600"/>
+          </div>
+          <div className="p-2 md:p-4">
+            <BiLogoNetlify className="text-5xl md:text-7xl text-teal-400"/>
+          </div>
         </div>
-
-
       </div>
+      <div className="flex justify-center mb-10">
+        <Link>
+          <button class="p-3 rounded-lg bg-blue-600 text-xl text-white hover:brightness-125 transition-all duration-150 text-center">Check my projects</button>
+        </Link>
+      </div>
+        
     </section>
   )
 }
