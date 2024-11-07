bb5fdcd28f358cbab569acacd62dd455fc03bb0a
JuanDavidDV <juandadazav@gmail.com>
2024-11-07 16:18:29 -0500

    animations completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { RiReactjsLine } from "react-icons/ri";
 import github from "../assets/images/github.png";
 import linkedin from "../assets/images/linkedIn.png";
@@ -17,12 +18,43 @@ import { SiPostman } from "react-icons/si";
 import { GrHeroku } from "react-icons/gr";
 import { BiLogoNetlify } from "react-icons/bi";
 
+const containerVariants = {
+  hidden: { opacity: 0, x: -100},
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    }
+  }
+};
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5 }}
+};
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+
+  }
+})
+
 const About = () => {
   return (
-    <section className="pg-4 lg:mb-36">
-      <div class="flex flex-col min-h-screen items-center justify-center bg-gradient-to-tr to-blue-900 from-emerald-500 px-10">
-        <div class="w-max md:py-10">
-          <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl md:text-6xl text-white font-bold">
+    <section className="pg-4 lg:mb-24">
+      <div className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-tr to-blue-900 from-emerald-500 px-10">
+        <div className="w-max md:py-10">
+          <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl md:text-6xl text-white font-bold">
             Hello World
           </h1>
         </div>
@@ -33,9 +65,11 @@ const About = () => {
                 I am Juan David Daza Velosa
             </h1>
           </div>
-          <p className="mt-3 text-white md:text-xl ">
-            Based in <span className="font-bold">Canada</span> 🍁. I am a passionate <span className="font-bold">Software Developer</span>, specialized in <span className="font-bold">Full-stack Development</span> through hands-on learning, and building real-world applications.   
-          </p>
+          <motion.div initial="hidden" animate="visible" variants={containerVariants}>
+            <p className="mt-3 text-white md:text-xl ">
+              Based in <span className="font-bold">Canada</span> 🍁. I am a passionate <span className="font-bold">Software Developer</span>, specialized in <span className="font-bold">Full-stack Development</span> through hands-on learning, and building real-world applications.   
+            </p>
+          </motion.div>
         </div>
         <div className="pt-10 flex gap-10 ">
           <a href="https://github.com/JuanDavidDV" target="_blank">
@@ -46,60 +80,68 @@ const About = () => {
           </a>
         </div>
       </div>
-      <div className="pb-24">
-        <h2 className="mt-20 mb-10 text-center text-4xl md:text-5xl text-gray-800 font-bold">
-          Technologies
-        </h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="p-2 md:p-2 md:p-4">
-            <SiRubyonrails className="text-5xl md:text-7xl text-red-500"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <RiReactjsLine className="text-5xl md:text-7xl text-cyan-400" />
-          </div>
-          <div className="p-2 md:p-4">
-            <SiJavascript className="text-5xl md:text-7xl text-yellow-400"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <SiMicrosoftsqlserver className="text-5xl md:text-7xl text-red-400" />
-          </div>
-          <div className="p-2 md:p-4">
-            <BiLogoPostgresql className="text-5xl md:text-7xl text-blue-800" />
-          </div>
-          <div className="p-2 md:p-4">
-            <FaNodeJs className="text-5xl md:text-7xl text-green-500" />
-          </div>
-          <div className="p-2 md:p-4">
-            <SiExpress className="text-5xl md:text-7xl"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <IoLogoGithub className="text-5xl md:text-7xl" />
-          </div>
-          <div className="p-2 md:p-4">
-            <FaGitAlt className="text-5xl md:text-7xl text-orange-600" />
-          </div>
-          <div className="p-2 md:p-4">
-            <FaHtml5 className="text-5xl md:text-7xl text-orange-500" />
-          </div>
-          <div className="p-2 md:p-4">
-            <IoLogoCss3 className="text-5xl md:text-7xl text-blue-600"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <FaSass className="text-5xl md:text-7xl text-pink-500"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <RiTailwindCssFill className="text-5xl md:text-7xl text-cyan-700"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <SiPostman className="text-5xl md:text-7xl text-orange-500" />
-          </div>
-          <div className="p-2 md:p-4">
-            <GrHeroku className="text-5xl md:text-7xl text-purple-600"/>
-          </div>
-          <div className="p-2 md:p-4">
-            <BiLogoNetlify className="text-5xl md:text-7xl text-teal-400"/>
-          </div>
-        </div>
+      <div className="pb-5">
+        <motion.h2
+          whileInView={{ opacity: 1, y:0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 1.5 }}
+          className="mt-20 mb-10 text-center text-4xl md:text-5xl text-gray-800 font-bold">
+            Technologies
+        </motion.h2>
+        <motion.div 
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div initial="initial" animate="animate" variants={iconVariants(2.5)} className="p-2 md:p-2 md:p-4">
+              <SiRubyonrails className="text-5xl md:text-7xl text-red-500"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(3)} className="p-2 md:p-4">
+              <RiReactjsLine className="text-5xl md:text-7xl text-cyan-400" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(5)} className="p-2 md:p-4">
+              <SiJavascript className="text-5xl md:text-7xl text-yellow-400"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(2)} className="p-2 md:p-4">
+              <SiMicrosoftsqlserver className="text-5xl md:text-7xl text-red-400" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(5)} className="p-2 md:p-4">
+              <BiLogoPostgresql className="text-5xl md:text-7xl text-blue-800" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(6)} className="p-2 md:p-4">
+              <FaNodeJs className="text-5xl md:text-7xl text-green-500" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(4)} className="p-2 md:p-4">
+              <SiExpress className="text-5xl md:text-7xl"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(2.5)} className="p-2 md:p-4">
+              <IoLogoGithub className="text-5xl md:text-7xl" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(3)} className="p-2 md:p-4">
+              <FaGitAlt className="text-5xl md:text-7xl text-orange-600" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(6)} className="p-2 md:p-4">
+              <FaHtml5 className="text-5xl md:text-7xl text-orange-500" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(5)} className="p-2 md:p-4">
+              <IoLogoCss3 className="text-5xl md:text-7xl text-blue-600"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(2.5)} className="p-2 md:p-4">
+              <FaSass className="text-5xl md:text-7xl text-pink-500"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(3)} className="p-2 md:p-4">
+              <RiTailwindCssFill className="text-5xl md:text-7xl text-cyan-700"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(5)} className="p-2 md:p-4">
+              <SiPostman className="text-5xl md:text-7xl text-orange-500" />
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(3.5)} className="p-2 md:p-4">
+              <GrHeroku className="text-5xl md:text-7xl text-purple-600"/>
+            </motion.div>
+            <motion.div initial="initial" animate="animate" variants={iconVariants(6)} className="p-2 md:p-4">
+              <BiLogoNetlify className="text-5xl md:text-7xl text-teal-400"/>
+            </motion.div>
+          </motion.div>
       </div>        
     </section>
   )
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,20 +1,34 @@
 import { EXPERIENCE } from "../constants";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Experience = () => {
   return (
     <main>
       <div className="pb-4 mx-10">
-        <h2 className="my-20 text-center text-4xl md:text-5xl text-gray-800 font-bold">Experience</h2>
+        <motion.h2
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.5 }}
+          className="my-20 text-center text-4xl md:text-5xl text-gray-800 font-bold">Experience</motion.h2>
         <div>
           {EXPERIENCE.map((experience, index) => (
             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-              <div className="w-full lg:w-1/4">
+              <motion.div 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="w-full lg:w-1/4">
                 <p className="mb-2 text-sm text-slate-700">
                   {experience.year}
                 </p>
                 <img width={150} height={100} className="mb-3 rounded" src={experience.image}/>
-              </div>
-              <div className="w-full max-w-xl lg:w-3/4">
+              </motion.div>
+              <motion.div 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1 }}
+                className="w-full max-w-xl lg:w-3/4">
                 <h3 className="mb-2 font-semibold">{experience.role} -{" "} 
                   <span className="text-sm text-slate-600">
                     {experience.company}  
@@ -27,10 +41,13 @@ const Experience = () => {
                     </li>
                   ))}
                 </ul>
-              </div>    
+              </motion.div>    
             </div>
           ))}
         </div>
+        <Link to="/contact">
+          <button className="p-3 rounded-lg bg-blue-600 text-xl text-white hover:brightness-125 transition-all duration-150 text-center">Let's connect! Contact me</button>
+        </Link>
       </div>
     </main>
   )
