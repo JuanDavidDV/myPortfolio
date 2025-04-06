95505ca109c5149408ded34dbef11d5277346d88
JuanDavidDV <juandadazav@gmail.com>
2025-04-05 23:50:37 -0400

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,22 @@ import { MdOutlineEmail } from "react-icons/md";
 const Footer = () => {
   return (
     <footer className="px-5 bg-black">
-      <div className="flex justify-between py-10">
+      <div className="flex flex-wrap justify-between items-center gap-5 py-10 min-w-0">
         <div>
           <p className="text-xs text-gray-200">Created by <strong>Juan David</strong> 2025 ©. All rights reserved.</p>
         </div>
         <div className="flex gap-5 justify-center items-center">
-          <a href="https://github.com/JuanDavidDV" target="_blank">
-            <img src={github} className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] flex justify-center items-center rounded-full"/>
+          <a href="https://github.com/JuanDavidDV" target="_blank" className="flex-shrink-0">
+            <img src={github} className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] rounded-full"/>
           </a>
-          <a href="https://www.linkedin.com/in/juan-david-dv/" target="_blank">
-            <img src={linkedin} className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] flex justify-center items-center rounded-full"/>
+          <a href="https://www.linkedin.com/in/juan-david-dv/" target="_blank" className="flex-shrink-0">
+            <img src={linkedin} className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] rounded-full"/>
           </a>
           <a href="mailto:juandadazav@gmail.com">
-            <MdOutlineEmail className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] flex justify-center items-center rounded-full" />
+            <MdOutlineEmail className="p-0.5 opacity-60 hover:opacity-100 bg-white w-[25px] h-[25px] rounded-full"/>
           </a>
         </div>
       </div>
-      
     </footer>
   )
 };
